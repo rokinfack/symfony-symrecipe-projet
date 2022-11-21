b75dedb8f841a7a3a4eaf6b62e21f627a63d632f
rostand kinfack <kinfackrostand@gmail.com>
2022-11-22 00:22:54 +0100

    add homePage

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+nameSpace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+
+
+    #[Route('/', name:"home.index")]
+    public function index():Response
+    {
+        return $this->render("home/index.html.twig");
+    }
+}
